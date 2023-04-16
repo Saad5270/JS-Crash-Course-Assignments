fac7cbb49070e6f1ea2f66b084c92fc65fb0588b
Saad <saadibrahimrock1000@gmail.com>
2023-04-16 22:45:11 +0500

    main logic completed

--- assignment3.js
+++ assignment3.js
@@ -6,47 +6,63 @@
     const year = document.getElementById("yearId");
     const languages = document.getElementById("languagesId");
     // const ratings = document.getElementById("ratingsId");
-
-
+    const movieListNames= document.getElementById("movie-names");
+    const movieYear= document.getElementById("year-released");
 
     const btn = document.getElementById("searchBtn")
 
-    
-    function search(){
-        const genreValue = genre.value;
-        const yearValue = year.value;
-        const languagesValue = languages.value;
+    function displayValues(result){
+      movieListNames.innerHTML = "";
+      movieYear.innerHTML = "";
 
-        
-        // const ratingsValue = ratings.value;
+      result.forEach(function (userValue) {
+      
+      const liName = document.createElement("li");
+      const liYear = document.createElement("li")
 
-        const result = data.filter(function(userValue) {  
-            if (typeof userValue.genres === 'string') {    
-              if (userValue.genres.toLowerCase().includes(genreValue.toLowerCase()) && userValue.release_date.includes(yearValue)
-              && userValue.original_language.toLowerCase().includes(languagesValue.toLowerCase())){
-                return true;
-              }
-            }
-            return false;
-          });
+      const movieNames = `
+          <h2 class="title">${userValue.title}</h2>
+          
+      `;
 
-        // const test = data.filter(function (userValue){
-        //     if (typeof userValue.release_date.toLowerCase().includes(yearValue.toLowerCase()) === 'string') { 
-        //         return true;
+      const movieYearRelease = `
+        <h2 class="title">${userValue.release_date.slice(0, 4)}</h2>
+      `;
+      liName.innerHTML = movieNames;
+      liYear.innerHTML = movieYearRelease;
+      
+      movieListNames.appendChild(liName);
+      movieYear.appendChild(liYear)
+    })
+      
 
-        //     }
-        //     else{
-        //         return false;
-        //     }
-
-        // });
-
-    //    console.log(test);
-        console.log(typeof yearValue,typeof genreValue, typeof languagesValue)
-        
-        
     }
 
+    function search(){
+        let genreValue = genre.value;
+        let yearValue = year.value;
+        let languagesValue = languages.value;
+        // let ratingsValue = ratings.value;
+        
+        const result = data.filter(function(userValue) {
+          if (typeof userValue.genres === 'string') {
+            return userValue.genres.toLowerCase().includes(genreValue.toLowerCase()) || userValue.release_date.includes(yearValue)
+            || userValue.original_language.toLowerCase().includes(languagesValue.toLowerCase());
+          
+            //the condition should be with && operator but it is not working for me
+              
+          }else{
+            return false;
+          }
+          
+        });
+
+        displayValues(result)
+    }
+
+    
     btn.addEventListener("click",search);
+    
+    
 
 })()
